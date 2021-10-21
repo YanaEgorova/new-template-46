da2e037e713ce2e50f475c09935c95dcfcec4e4c
Yana <yanaegorova69.3@gmail.com>
2021-10-21 20:40:54 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,292 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Hamster Bed',
+        price: 19.99,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Size:8”*3”*3.5” Suitable for small pet eg. Sugar glider, Squirrel, Hamster, Mouse and other small pets Pls check the size before you buy it.',
+            },
+            {
+                text: 'Made by soft and warm fleece .Make the pet feel comfortable and warm.',
+            },
+            {
+                text: 'Could hanging On the cage.It could be a hammock.',
+            },
+            {
+                text: 'Nice shed for its heat preservation to keep your small pet away from the cold, especially in winter.',
+            },
+            {
+                text: 'Super cute banana outlook,bright color,your pet will like it.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png', './img/img1-4.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Hamster Exercise Wheel',
+        price: 24.99,
+        image: './img/img2.png',
+        description: [
+            {
+                title: 'Mini Size for Your Pets',
+                text: 'This is S size for small furry animals, about 6.1x3.74x7 inch(15.5x9.5x18 cm), 6.1 inch(15.5 cm) diameter drum wheel, with 1.97 inch(5 cm) track wide. Highly recommended for dwarf hamsters, small gerbil, mice, and other similar-sized pets, fits for them when run without bending their backs.',
+            },
+            {
+                title: 'Safe & Chew Out Material',
+                text: 'Made of high quality wood, which are durable and safe chew out. It not only serves as exercise toy preferred for small pet sports activities but also as chew toy, offers excellent pet teeth grinding effects.',
+            },
+            {
+                title: 'Completely Noiseless',
+                text: 'Designed by wooden with a very low noise level (around 40dB) and dual ball bearings, so can quiet keep it on spinning smoothly, even your little friends working out on the hamster exercise wheel in the night time, you still have a sweet sleep.',
+            },
+            {
+                title: 'Reliable&Stable Support',
+                text: 'The non-tipping stand gives this wheel a firm stand, widened and thickened wood base greatly avoids reversal through providing reliable and stable mount for the exercise wheel.',
+            },
+            {
+                title: 'Must-have Hamster Cage Accessories',
+                text: 'Helps your hamster exercise more and develop balance ability and agility. Your furry critter will love this hamster wheel toy as Hamsters love the hamster rolling wheel like a runner to help them to broken boredom & enrich their nighttime, also keep them to exercise for maintaining their strength and well-being.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png',],
+    },
+    {
+        id: 'product-3',
+        name: 'Hide-Away for Reptiles',
+        price: 35.32,
+        image: './img/img3.png',
+        description: [
+            {
+                title: 'Macabre Meets Desert-Punk',
+                text: 'The Reptology Skull Hide-Away is a compliment to any themed tank or terrarium, or serves as a focal point to your pet reptile’s barren waste land. Finely crafted with realistic details. Perfect size for Anoles, Bearded Dragons, Geckos, Iguanas, Hermit Crabs, and many more!',
+            },
+            {
+                title: 'Great For Reptiles Who Are Shy Or Social',
+                text: 'This hide-away is a must have for pet reptiles who love to hide, or who like privacy at times. On the other hand, this decoration is also suitable for sociable reptiles who love to climb!',
+            },
+            {
+                title: 'For Small & Medium Reptiles',
+                text: 'This ornament features 3 holes: 2 on the side, and the open mouth. Side Hole #1 Measurements: (5.5” x 4.25”); Side Hole #2 Measurements (4.5” x 4”); Open Mouth Measurements (6” x 3.5”).',
+            },
+            {
+                title: 'Versatile & Strong',
+                text: 'The Reptology Skull Hide-Away is made of safe and durable resin. This material is scratch resistant, meaning that your pet won’t damage the decoration or hurt itself. At the same time, the resin won’t irritate or harm your pet’s skin.',
+            },
+            {
+                title: 'Overall Measurements',
+                text: '10.25” (H) x 9” (D) x 8” (W).',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Grooming Kit',
+        price: 39.99,
+        image: './img/img4.png',
+        description: [
+            {
+                text: '7-piece assortment of grooming essentials in a durable nylon tote bag.',
+            },
+            {
+                text: 'Tote features six exterior pockets for added storage.',
+            },
+            {
+                text: 'Features a convenient top Drawstring closure and plastic feet that protect bottom of bag.',
+            },
+            {
+                text: 'Comfortable webbing handles, and an adjustable, padded shoulder strap.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png', './img/img4-5.png', './img/img4-6.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Milk Bowl for Puppies',
+        price: 43.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Name: pet bionic self feeding device.',
+            },
+            {
+                text: 'Material: food grade silicone.',
+            },
+            {
+                text: 'Number of teats: 5.',
+            },
+            {
+                text: 'Feature: Let the pet baby maintain the original drinking position and suck independently.',
+            },
+            {
+                text: 'Applicable objects: Suitable for pet babies new born.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png', './img/img5-4.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Pet Jumpsuit',
+        price: 49.95,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Ultra-light - 33% lighter compare to competitive product.',
+            },
+            {
+                text: 'All hand-made with care.',
+            },
+            {
+                text: '2 way zipper.',
+            },
+            {
+                text: 'Pocketable design with a pouch.',
+            },
+            {
+                text: 'Size: Medium: 11.0" Neck, 16.5" Chest, 10.6" Product Length.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png','./img/img6-3.png'],
+    },
+    {
+        id: 'product-7',
+        name: 'Hoof Trimmers',
+        price: 69.99,
+        image: './img/img7.png',
+        description: [
+            {
+                text: 'Carbon Steel Alloy Multi-purpose Hoof Trimmers, a good helper tool for the farmer or veterinary.',
+            },
+            {
+                text: '100% High-quality products. 16.7(In) 4.9(lb). Maximum Opening: 3(cm)/1.18(in).',
+            },
+            {
+                text: 'Suitable for Goats Sheep Cattle Horses and other Large Animals.',
+            },
+            {
+                text: 'Simple to Operate, High Performing, and Cost-effective. Easy to use and convenient.',
+            },
+            {
+                text: 'Package Includes: 1x Hoof Trimmers Black Heavy Duty Nippers.',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.png', './img/img7-3.png', './img/img7-4.png', './img/img7-5.png'],
+    },
+    {
+        id: 'product-8',
+        name: 'Clipper Kit',
+        price: 79.99,
+        image: './img/img8.png',
+        description: [
+            {
+                text: 'Designed for trimming faces, ears, and paws on all dog breeds and occasional body clipping on light to medium breeds,  rechargeable cordless clipper offers substantial power and speed without producing additional heat or noise.',
+            },
+            {
+                text: 'The Lithium Ion battery allows for 2 hours of cordless run time. The 15-minute quick charge feature provides an extra 10 minutes of clipping for every 15 minutes charged.',
+            },
+            {
+                text: 'The Clipper also features an easy-to-adjust standard #30-15-10 adjustable blade; operates at a powerful 6,000 strokes per minute, making clipping fast and easy.',
+            },
+            {
+                text: 'Measures 7.25 by 2 by 1.25 inches (H x W x D); weighs 10.3 ounces; includes the Cordless Clipper, storage case, charging cord, styling comb, cleaning brush, blade oil, stainless steel scissors, and attachment guide combs.',
+            },
+        ],
+        images: ['./img/img8-1.png', './img/img8-2.png', './img/img8-3.png', './img/img8-4.png'],
+    },
+    {
+        id: 'product-9',
+        name: 'Pet Carrier',
+        price: 84.41,
+        image: './img/img9.png',
+        description: [
+            {
+                text: 'Durable 600 Denier Polyester and Feather Light Construction.',
+            },
+            {
+                text: 'Adjustable 4-point padded shoulder strap and leather hand grip.',
+            },
+            {
+                text: 'Retractable privacy flaps on all mesh windows.',
+            },
+            {
+                text: 'Zipper security clips and a tether for safety and security is included.',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.png', './img/img9-3.png', './img/img9-4.png', './img/img9-5.png', './img/img9-6.png'],
+    },
+    {
+        id: 'product-10',
+        name: 'Pet Sofa',
+        price: 89.99,
+        image: './img/img10.png',
+        description: [
+            {
+                text: '1.5” legs lift bed off floor, keeping your pet draft free, Perfect for pets who like to curl or lean when they sleep.',
+            },
+            {
+                text: 'Storage pocket for toys and bones.',
+            },
+            {
+                text: 'Removable/washable cushion cover, Easy to spot clean all other surfaces with a mild detergent.',
+            },
+            {
+                text: 'Fits pets up to 10 lbs, Pet sofa size is 26" x 16" x 11"',
+            },
+            {
+                text: 'Cushion is 12.75" x 19.5" x 3". The height from the floor to the top of the cushion is 6.75"',
+            },
+        ],
+        images: ['./img/img10-1.png', './img/img10-2.png'],
+    },
+    {
+        id: 'product-11',
+        name: 'Terrariums Humidifier',
+        price: 119.98,
+        image: './img/img11.png',
+        description: [
+            {
+                title: '2.5l - Large Size Water Tank',
+                text: 'It is up to 2.5 liters of water can be accommodated, include an adjustable (1.3-5.3 feet) length hose and two suction feet for securing the hose.',
+            },
+            {
+                title: 'Electrical Safety',
+                text: 'Check if your electricity voltage is 110-220v before use. It can work continuously, but if it\'s dry of water in the tank , the machine\'s indicator light changes from green to red, and it will automatically stop working.',
+            },
+            {
+                title: 'Adjustable Mist Output',
+                text: 'There is a rotary button on the front of the machine, and the amount of mist can be adjusted arbitrarily according to needs.',
+            },
+            {
+                title: 'Cleaning Cycle',
+                text: 'It is recommended to clean the water tank every 2 or 3 weeks, and the machine base needs to be cleaned once a week to ensure that the machine is not blocked and can work normally.',
+            },
+        ],
+        images: ['./img/img11-1.png', './img/img11-2.png', './img/img11-3.png', './img/img11-4.png', './img/img11-5.png'],
+    },
+    {
+        id: 'product-12',
+        name: 'Dog Collar',
+        price: 148.00,
+        image: './img/img12.png',
+        description: [
+            {
+                text: 'Made from Top Quality European Leather.',
+            },
+            {
+                text: 'Brass Plates and Brass buckles and D-rings.',
+            },
+            {
+                text: 'Available in Different sizes.',
+            },
+            {
+                text: 'Colors: Black, Brown, Tan, Pink, White; Fits Neck 34-Inch To 38-Inch.',
+            },
+            {
+                text: '36" x 1-1/2"',
+            },
+        ],
+        images: ['./img/img12-1.png','./img/img12-2.png'],
+    },
+]
